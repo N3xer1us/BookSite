8093ad335720dd8c699d95d387cd787a827908fe
N3xer1us <66820036+N3xer1us@users.noreply.github.com>
2020-06-12 11:43:27 +0300

    Add files via upload

---
+++ Repositories/Repos/BaseRepo.cs
@@ -0,0 +1,71 @@
+﻿using Data;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repos
+{
+   public class BaseRepo<T> 
+        where T : BaseEntity
+    {
+
+        internal BookSiteDBContext db;
+        internal DbSet<T> Items;
+
+        public BaseRepo(BookSiteDBContext db)
+        {
+            this.db = db;
+            this.Items = db.Set<T>();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter=null)
+        {
+            IQueryable<T> query = Items;
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            return query.ToList();
+        }
+
+        public T GetById(int Id)
+        {
+            return Items.Where(u => u.Id == Id).FirstOrDefault();
+        }
+
+        public void Insert(T item)
+        {
+            Items.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            Items.Attach(item);
+            DbEntityEntry entry = db.Entry(item);
+            entry.State = EntityState.Modified;
+        }
+
+        public virtual void Delete(int id)
+        {
+            T item = Items.Find(id);
+            Delete(item);
+        }
+
+
+        public virtual void Delete(T entityToDelete)
+        {
+            if (db.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                Items.Attach(entityToDelete);
+            }
+            Items.Remove(entityToDelete);
+        }
+
+    }
+}
---
+++ Repositories/Repos/UnitOfWork.cs
@@ -0,0 +1,144 @@
+﻿using Data;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repos
+{
+    public class UnitOfWork
+    {
+
+        #region PrivateRepos
+        private BookSiteDBContext db = new BookSiteDBContext();
+        private BaseRepo<User> userRepo;
+        private BaseRepo<Book> bookRepo;
+        private BaseRepo<Author> authorRepo;
+        private BaseRepo<Genre> genreRepo;
+        private BaseRepo<Role> roleRepo;
+        private BaseRepo<UserToBook> userToBookRepo;
+        private BaseRepo<Friend> friendRepo;
+        private BaseRepo<FriendRequest> friendRequestRepo;
+        #endregion
+
+        #region PublicRepos
+        public BaseRepo<User> UserRepo
+        {
+            get
+            {
+
+                if (this.userRepo == null)
+                {
+                    this.userRepo = new BaseRepo<User>(db);
+                }
+                return userRepo;
+            }
+        }
+        public BaseRepo<Book> BookRepo
+        {
+            get
+            {
+                if (this.bookRepo == null)
+                {
+                    this.bookRepo = new BaseRepo<Book>(db) ;
+                }
+                return bookRepo;
+            }
+        }
+        public BaseRepo<Author> AuthorRepo
+        {
+            get
+            {
+                if (this.authorRepo == null)
+                {
+                    this.authorRepo = new BaseRepo<Author>(db);
+                }
+                return authorRepo;
+            }
+        }
+        public BaseRepo<Genre> GenreRepo
+        {
+            get
+            {
+                if (this.genreRepo == null)
+                {
+                    this.genreRepo = new BaseRepo<Genre>(db);
+                }
+                return genreRepo;
+            }
+        }
+        public BaseRepo<Role> RoleRepo
+        {
+            get
+            {
+                if (this.roleRepo == null)
+                {
+                    this.roleRepo = new BaseRepo<Role>(db);
+                }
+                return roleRepo;
+            }
+        }
+        public BaseRepo<UserToBook> UserToBookRepo
+        {
+            get
+            {
+                if(this.userToBookRepo==null)
+                {
+                    this.userToBookRepo = new BaseRepo<UserToBook>(db);
+                }
+                return userToBookRepo;
+            }
+        }
+        public BaseRepo<Friend> FriendRepo
+        {
+            get
+            {
+                if (this.friendRepo == null)
+                {
+                    this.friendRepo = new BaseRepo<Friend>(db);
+                }
+                return friendRepo;
+            }
+        }
+        public BaseRepo<FriendRequest> FriendRequestRepo 
+        {
+            get
+            {
+                if (this.friendRequestRepo == null)
+                {
+                    this.friendRequestRepo = new BaseRepo<FriendRequest>(db);
+                }
+                return friendRequestRepo;
+            }
+        }
+        #endregion
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
